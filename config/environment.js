a806bf2d7cde4262335aca17066279e60eebc353
Barrett K. Harber <barrett.harber@gmail.com>
2017-06-30 14:52:54 -0400

    Update eslint and related packages

--- config/environment.js
+++ config/environment.js
@@ -8,36 +8,36 @@ module.exports = function(environment) {
         locationType: 'auto',
         EmberENV: {
             FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
+                // Here you can enable experimental features on an ember canary build
+                // e.g. 'with-controller': true
             },
         },
 
-    // User-specific configuration
+        // User-specific configuration
         sentry: {
             dsn: process.env.SENTRY_DSN || '',
-        //cdn: 'https://cdn.ravenjs.com/3.5.1/ember/raven.min.js'
+            // cdn: 'https://cdn.ravenjs.com/3.5.1/ember/raven.min.js'
         },
 
         APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+            // Here you can pass flags/options to your application instance
+            // when it is created
         },
     };
 
     if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+        // ENV.APP.LOG_RESOLVER = true;
+        // ENV.APP.LOG_ACTIVE_GENERATION = true;
+        // ENV.APP.LOG_TRANSITIONS = true;
+        // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+        // ENV.APP.LOG_VIEW_LOOKUPS = true;
     }
 
     if (environment === 'test') {
-    // Testem prefers this...
+        // Testem prefers this...
         ENV.locationType = 'none';
 
-    // keep test console output quieter
+        // keep test console output quieter
         ENV.APP.LOG_ACTIVE_GENERATION = false;
         ENV.APP.LOG_VIEW_LOOKUPS = false;
 
